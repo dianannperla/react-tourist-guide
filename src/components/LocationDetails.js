0f283976562a4f221beccd28c47c6194fb88d53a
dianannperla <waafirmelyza@hotmail.com>
2025-09-28 14:33:21 +0100

    feat: initial commit for src/components/LocationDetails.js

---
+++ src/components/LocationDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LocationDetails = () => {
+  return (
+    <div>
+      <h1>Chi Tiết Địa Điểm</h1>
+      <p>Thông tin về địa điểm sẽ được hiển thị tại đây.</p>
+    </div>
+  );
+};
+
+export default LocationDetails;
